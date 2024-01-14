5d841db28333aeaa662166b278d6ea0e530d7fe2
Gupta5804 <gupta.rishabh.abcd@gmail.com>
2024-01-14 09:47:47 +0530

    sd

--- app/main/views.py
+++ app/main/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+
 def home(request):
     return render(request, "home.html")
+
+
 class index(APIView):    
     def get(self, request, format=None):    
         return Response('Hello World!')
+    
