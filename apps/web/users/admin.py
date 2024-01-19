f0d9064bb09bb67fcabbedbac24acb7a5983bb0a
Gupta5804 <gupta.rishabh.abcd@gmail.com>
2024-01-19 07:08:59 +0530

    sds

--- apps/web/users/admin.py
+++ apps/web/users/admin.py
@@ -3,6 +3,6 @@ from .models import CustomUser,UserCustomer,UserShop
 
 admin.site.register(CustomUser)
 admin.site.register(UserShop)
-admin.site.register(UserShop)
+admin.site.register(UserCustomer)
 
 # Register your models here.
