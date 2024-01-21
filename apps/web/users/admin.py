cbf9c1fd5d535b88de9cdc4d033e1ae4967f939b
Gupta5804 <gupta.rishabh.abcd@gmail.com>
2024-01-21 09:16:46 +0530

    sds

--- apps/web/users/admin.py
+++ apps/web/users/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, CustomerProfile, ShopProfile
 
-from .models import CustomUser,UserCustomer,UserShop
+class CustomerProfileInline(admin.StackedInline):
+    model = CustomerProfile
+    can_delete = False
+    verbose_name_plural = 'Customer Profile'
 
-admin.site.register(CustomUser)
-admin.site.register(UserShop)
-admin.site.register(UserCustomer)
+class ShopProfileInline(admin.StackedInline):
+    model = ShopProfile
+    can_delete = False
+    verbose_name_plural = 'Shop Profile'
 
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['email', 'is_staff', 'is_active','user_type']
+    ordering = ('email','is_staff','is_active','user_type')
+    search_fields = ['email']
+    fieldsets = (
+        (None, {'fields': ('email', 'password','user_type')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_active', 'is_staff', 'is_superuser')}
+        ),
+    )
+    inlines = [CustomerProfileInline, ShopProfileInline]
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- apps/web/users/models.py
+++ apps/web/users/models.py
@@ -1,74 +1,53 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser , PermissionsMixin
 
-
-class UserAccountManager(BaseUserManager):
-    def create_user(self,email, password=None, **kwargs):  # kwargs for first_name,last_name dictionary
-        """
-        Creates and saves a User with the given email, date of
-        birth and password.
-        """
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError("Users must have an email address")
-        
-        email = self.normalize_email(email) # converts capital letters to small letters after the @
-        email = email.lower()
-
-        user = self.model(
-            email=email,
-            **kwargs
-        )
-
-        user.set_password(password) # hash the password
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None,**kwargs):
-        
-        user = self.create_user(
-            email,
-            password=password,
-            **kwargs
-            
-        )
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
 
 
-class CustomUser(AbstractBaseUser,PermissionsMixin):
-    # first_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True,max_length=255)
-    # date_of_birth = models.DateField()
-    
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    USER_TYPES = (
+        ('customer', 'Customer'),
+        ('shop', 'Shop'),
+    )
+    email = models.EmailField(unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-
-    objects = UserAccountManager()
+    user_type = models.CharField(max_length=20, choices=USER_TYPES,default='customer')
+    objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.email
 
-class UserCustomer(models.Model):
-    user = models.OneToOneField(CustomUser,on_delete = models.CASCADE,primary_key=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    address = models.CharField(max_length=120)
-    dob = models.CharField(max_length=50)
-    contact_no = models.CharField(max_length=20)
-    alternative_no = models.CharField(max_length=20)
-    
-
-    
-class UserShop(models.Model):
+class CustomerProfile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
-    shop_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=30, blank=True, null=True)
+    address = models.CharField(max_length=120, blank=True, null=True)
+    dob = models.CharField(max_length=50, blank=True, null=True)
+    contact_no = models.CharField(max_length=20, blank=True, null=True)
+    alternative_no = models.CharField(max_length=20, blank=True, null=True)
+
+class ShopProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+    shop_name = models.CharField(max_length=50, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
-    about_shop = models.CharField(max_length=500)
-    contact_no = models.CharField(max_length=20)
-    alternative_no = models.CharField(max_length=20)
+    about_shop = models.CharField(max_length=500, blank=True, null=True)
+    contact_no = models.CharField(max_length=20, blank=True, null=True)
+    alternative_no = models.CharField(max_length=20, blank=True, null=True)
