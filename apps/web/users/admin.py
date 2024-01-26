c575729a6119672a578b4f916fcd2310f53a6af4
Gupta5804 <gupta.rishabh.abcd@gmail.com>
2024-01-26 14:21:20 +0530

    sds

--- apps/web/users/admin.py
+++ apps/web/users/admin.py
@@ -4,8 +4,14 @@ from .models import CustomUser, ShopProfile
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ['email', 'user_type', 'is_staff']
-    ordering = ('email', 'user_type', 'is_staff')
+    list_display = ['email', 'user_type', 'is_active', 'is_staff']
+    ordering = ('email', 'user_type', 'is_active', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal Info'), {'fields': ('email','first_name','last_name', 'address', 'dob','user_type', 'contact_no', 'alternative_no')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
 
 admin.site.register(CustomUser, CustomUserAdmin)
 
