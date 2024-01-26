e7acbb1d8bfbfb6909f933b6c32c7ad298412b0d
Gupta5804 <gupta.rishabh.abcd@gmail.com>
2024-01-26 14:30:14 +0530

    sds

--- apps/web/users/admin.py
+++ apps/web/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, ShopProfile
+from django.utils.translation import gettext_lazy as _
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
