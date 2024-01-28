942e2967d68e588ddcbd0456097a217d5e11fa7d
Gupta5804 <gupta.rishabh.abcd@gmail.com>
2024-01-28 10:49:36 +0530

    sds

--- apps/www/app/activation/[uid]/[token]/page.tsx
+++ apps/www/app/activation/[uid]/[token]/page.tsx
@@ -1,4 +1,34 @@
-export default function Page(){
+'use Client';
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useActivationMutation } from "@/redux/features/authApiSlice";
+import { toast } from "react-toastify";
+interface Props {
+    params: {
+        uid:string;
+        token:string;
+    }
+}
+
+export default function Page({ params }: Props){
+    const router = useRouter();
+    const [activation] = useActivationMutation();
+    useEffect(() => {
+        const { uid, token } = params;
+
+        activation({ uid, token })
+            .unwrap()
+            .then(() => {
+                toast.success('Account Activated Successfully');
+            })
+            .catch(() => {
+                toast.error('Failed to activate account');
+            })
+            .finally(() =>{
+                router.push('/');
+            });
+    }, []);
     return (
         <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
             <div className='sm:mx-auto sm:w-full sm:mx-w-sm'>
