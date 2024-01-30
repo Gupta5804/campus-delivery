ea620c65d287270e928223453e1a205a2f18b82f
Gupta5804 <gupta.rishabh.abcd@gmail.com>
2024-01-30 13:54:57 +0530

    sds

--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -2,6 +2,18 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    
+  webpack: (config) => {
+    let modularizeImports = null;
+    config.module.rules.some((rule) =>
+      rule.oneOf?.some((oneOf) => {
+        modularizeImports =
+          oneOf?.use?.options?.nextConfig?.modularizeImports;
+        return modularizeImports;
+      }),
+    );
+    if (modularizeImports?.["@headlessui/react"])
+      delete modularizeImports["@headlessui/react"];
+    return config;
+  },
 }
 export default nextConfig
