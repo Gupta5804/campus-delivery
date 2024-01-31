5c1673d5892dec50314809fa18e1ab718498e309
Gupta5804 <gupta.rishabh.abcd@gmail.com>
2024-01-31 09:31:51 +0530

    sds

--- apps/www/hooks/index.ts
+++ apps/www/hooks/index.ts
@@ -1,4 +1,5 @@
 export { default as useRegister } from './use-register';
+export { default as useSocialAuth } from './use-social-auth';
 export { default as useVerify } from './use-verify';
 export { default as useLogin } from './use-login';
 export { default as useResetPassword } from './use-reset-password';
