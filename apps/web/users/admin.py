d229e3c09805cff359086ce31b26679d7992ebca
Gupta5804 <gupta.rishabh.abcd@gmail.com>
2024-02-16 12:55:05 +0530

    sds

--- apps/web/users/admin.py
+++ apps/web/users/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import CustomUser, ShopProfile
 from django.utils.translation import gettext_lazy as _
 
-class CustomUserAdmin(UserAdmin):
+class CustomUserAdmin(BaseUserAdmin):
     model = CustomUser
     list_display = ['email', 'user_type', 'is_active', 'is_staff']
     ordering = ('email', 'user_type', 'is_active', 'is_staff')
@@ -15,6 +15,23 @@ class CustomUserAdmin(UserAdmin):
     )
     search_fields = ['email', 'first_name', 'last_name']
     list_filter = ['user_type', 'is_active', 'is_staff']
+    exclude = ('username',)  # Exclude the 'username' field
+
+    # Explicitly set the fieldsets for add and change forms
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    # Explicitly set the fields for the change form
+    # Note: 'email' replaces 'username' here
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('first_name', 'last_name', 'address', 'dob', 'user_type', 'contact_no', 'alternative_no',)}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
 
 admin.site.register(CustomUser, CustomUserAdmin)
 
